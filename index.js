dadcef800e58a14d27546be1727c96e99f48fc10
moreenisaac <nkirotemoreen70@gmail.com>
2022-08-13 07:13:03 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,51 @@
+
+//function with results
+// Initialize add function
+function add(x, y) {
+	return x + y;
+}
+
+// Invoke function to find the sum
+add(9, 7);
+
+// Initialize subtract function
+function subtract(a, b) {
+	return a - b;
+}
+
+// Invoke function to find the subtraction
+subtract(2, 4);
+
+// Initialize multiply function
+function multiply(a, b) {
+	return a * b;
+}
+
+// Invoke function to find the multiplication
+multiply(2, 4);
+
+// Initialize multiply function
+function divide(a, b) {
+	return a / b;
+}
+
+// Invoke function to find the division
+divide(2, 4);
+
+//increment functions
+function increment(a){
+    return a + 1;
+}
+//decrement functions
+function decrement(a){
+    return a - 1;
+}
+
+//parse
+function makeInt(n){
+    return parseInt(n,10)
+  }
+  function preserveDecimal(n){
+    return parseFloat(n)
+  }
+
